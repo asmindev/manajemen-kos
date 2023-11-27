00eb78366141cae4a06fb2c5539ece16983715a2
Adell Cantik <aadelbudimann@gmail.com>
2023-11-27 18:24:35 +0700

    commit adel

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,42 @@
+# adel
+# app/routes.py
+
 import os
 from flask import Flask, request, jsonify
 from app.models import db, Property
 from flask_migrate import Migrate
-from dotenv import load_dotenv
 
-load_dotenv()
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
-
+app = Flask(_name_)
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    os.environ.get("DATABASE_URI") or "mysql://root:zett@localhost/property_db"
+)
 app.config.from_object("config.config")
 db.init_app(app)
 migrate = Migrate(app, db)
 
 
 # Route to get list of properties
-@app.route("/properties", methods=["GET"])
+@app.route("/api/properties", methods=["GET"])
 def get_properties():
     properties = Property.query.all()
     property_list = [
-        {"id": prop.id, "name": prop.name, "price": prop.price} for prop in properties
+        {
+            "id": prop.id,
+            "name": prop.name,
+            "price": prop.price,
+            "number": prop.room_number,
+            "image": prop.image_url,
+            "description": prop.description,
+            "status": prop.status,
+            "facilities": [i.strip() for i in prop.facilities.split(",")],
+        }
+        for prop in properties
     ]
     return jsonify(property_list)
 
 
 # Route to get details of a property
-@app.route("/properties/<int:property_id>", methods=["GET"])
+@app.route("/api/properties/<int:property_id>", methods=["GET"])
 def get_property(property_id):
     property = Property.query.get(property_id)
     if property:
@@ -32,9 +44,14 @@ def get_property(property_id):
             "id": property.id,
             "name": property.name,
             "price": property.price,
+            "number": property.room_number,
+            "image": property.image_url,
+            "description": property.description,
+            "status": property.status,
+            "facilities": [i.strip() for i in property.facilities.split(",")],
         }
         return jsonify(property_data)
     return jsonify({"message": "Property not found"}), 404
 
 
-# Add more routes and business logic as needed
+# Add more routes and business logic as needed
