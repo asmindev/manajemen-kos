a5545b444e40dfd3218a50459169370095d7ef75
GloryPower6 <1605azisnasir@gmail.com>
2023-11-27 19:59:18 +0800

    Update model User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,40 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         unique: true,
     },
+    userId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    phone_number: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "tenant"],
+        default: "tenant",
+        required: true,
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+    room: {
         type: String,
         required: true,
     },
