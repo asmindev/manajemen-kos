0571a58b96b17be3263fa9425de4f919505e8c02
CodenameRiq <thariqasyamil@gmail.com>
2023-11-27 19:17:21 +0700

    thoriq models

--- app/models.py
+++ app/models.py
@@ -5,6 +5,10 @@ db = SQLAlchemy()
 
 class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    # Add more fields as needed
+    room_number = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    status = db.Column(db.String(25))
+    image_url = db.Column(db.Text)
+    facilities = db.Column(db.Text)
