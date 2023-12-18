72e3416a490d2169b1d9c9b436ad96c92fa1b8c0
Asmindev <asmin987asmin@gmail.com>
2023-12-18 09:07:05 +0800

    fix

--- src/app/api/booking/create/route.js
+++ src/app/api/booking/create/route.js
@@ -4,13 +4,15 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
     try {
         const data = await request.json();
+        console.log(data)
         const resp = await axios.post(
-            "http://localhost:5002/api/booking/create",
+            "http://localhost:5002/api/bookings",
             { ...data }
         );
         // console.log(resp.data);
         return NextResponse.json(resp.data, { status: 200 });
     } catch (err) {
+        console.log({err})
         return NextResponse.json({ error: err.data }, { status: 500 });
     }
 }
